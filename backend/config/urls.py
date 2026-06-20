f4215247d64a941aedb80b6d1182b293d80000e8
Seira Elsa Biju <seira.droid@example.com>
2026-06-20 19:04:07 +0530

    feat: add drf spectacular swagger routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    # FIX: ALL API ROUTES LIVE HERE
     path("api/v1/", include("distress_app.urls")),
+
+    # OpenAPI Schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+
+    # Swagger UI
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ]
